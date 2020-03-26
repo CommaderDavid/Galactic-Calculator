f0f6efe402d949a182977327b683626bfc9615cc
David Boedigheimer <stealth259@gmail.com>
2020-03-26 15:21:08 -0700

    Add test for finding user age.

--- __tests__/galactic-calculator.test.js
+++ __tests__/galactic-calculator.test.js
@@ -1,1 +1,11 @@
 // use "debugger" to look for bugs and broken code.
+import { Calculate } from "./../src/galactic.js";
+
+describe('Calculate', () => {
+
+  test('should find and show user inputted age', () => {
+    let user = new Calculate(24);
+
+    expect(user.age).toEqual(24);
+  });
+});
---
+++ src/galactic.js
@@ -0,0 +1,9 @@
+export class Calculate {
+  constructor(age) {
+    this.age = age;
+  }
+
+  mercuryAge() {
+    this.age *= .24;
+  }
+}
