460173406e9aed2908285281841697663b5638d0
David Boedigheimer <stealth259@gmail.com>
2020-03-30 16:35:13 -0700

    Add final touches to UI.

--- src/main.js
+++ src/main.js
@@ -7,8 +7,31 @@ import { Calculate } from './galactic.js';
 $(document).ready(function() {
   $("form#mainForm").submit(function (e) {
     e.preventDefault();
+    const userAge = $("#userAge").val();
+    let inputtedAge = new Calculate(userAge);
     let typePlanet = $("#planets").val();
-    const inputtedAge = $("#userAge").val();
-    
+
+    const merAge = inputtedAge.mercuryAge();
+    const venAge = inputtedAge.venusAge();
+    const marAge = inputtedAge.marsAge();
+    const jupAge = inputtedAge.jupiterAge();
+
+    if (typePlanet === "1") {
+      $("#answer").show();
+      $("#planetAge").empty().append(merAge);
+      $("#yearsLeft").empty().append(inputtedAge.determinesLife(merAge));
+    } else if (typePlanet === "2") {
+      $("#answer").show();
+      $("#planetAge").empty().append(venAge);
+      $("#yearsLeft").empty().append(inputtedAge.determinesLife(venAge));
+    } else if (typePlanet === "3") {
+      $("#answer").show();
+      $("#planetAge").empty().append(marAge);
+      $("#yearsLeft").empty().append(inputtedAge.determinesLife(marAge));
+    } else if (typePlanet === "4") {
+      $("#answer").show();
+      $("#planetAge").empty().append(jupAge);
+      $("#yearsLeft").empty().append(inputtedAge.determinesLife(jupAge));
+    }
   });
 });
