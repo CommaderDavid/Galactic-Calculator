3f65f5e201bc467d8b4d23185b6cc05c9ee3f6d2
David Boedigheimer <stealth259@gmail.com>
2020-03-30 15:21:08 -0700

    Add start of UI for user.

--- src/main.js
+++ src/main.js
@@ -2,4 +2,13 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { nameOfFunction } from './';
+import { Calculate } from './galactic.js';
+
+$(document).ready(function() {
+  $("form#mainForm").submit(function (e) {
+    e.preventDefault();
+    let typePlanet = $("#planets").val();
+    const inputtedAge = $("#userAge").val();
+    
+  });
+});
